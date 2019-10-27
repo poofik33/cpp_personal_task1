71156267d219669ded7084ac2803d421dbc64e3c
Aleksandr Postnikov <sasha2011-1997@mail.ru>
2019-10-27 12:32:17 +0300

    Delete unused functions

--- PT2/input.c
+++ PT2/input.c
@@ -49,67 +49,6 @@ char* input_string() {
     return new_string;
 }
 
-struct tm * input_date() {
-    char* date_string_p = NULL;
-    char* tmp = input_string();
-    if (!tmp)
-        return NULL;
-    date_string_p = (char *)realloc(tmp, sizeof(char) * DATE__FORMAT_SIZE + 1);
-    date_string_p[DATE__FORMAT_SIZE] = '\0';
-    if(strlen(date_string_p) != DATE__FORMAT_SIZE) {
-        free(date_string_p);
-        return NULL;
-    }
-    tmp = date_string_p;
-    bool correct_date_input = 1;
-    char* str_int = (char *)malloc(sizeof(char) * (DAY_FORMAT + 1));
-    if (!str_int) {
-        free(date_string_p);
-        return NULL;
-    }
-    memcpy(str_int, tmp, 2);
-    str_int[DAY_FORMAT] = '\0';
-    int day = atoi(str_int);
-    correct_date_input = correct_date_input && (day <= MAX_DAY_VALUE || day >= MIN_DAY_VALUE);
-    tmp += DAY_FORMAT + 1;
-    memcpy(str_int, tmp, 2);
-    str_int[MONTH_FORMAT] = '\0';
-    int month = atoi(str_int);
-    correct_date_input = correct_date_input && (month <= MAX_MONTH_VALUE || month >= MIN_MONTH_VALUE);
-    tmp += MONTH_FORMAT + 1;
-    char* str_year = realloc(str_int, sizeof(char) * (YEAR_FORMAT + 1));
-    if (!str_year) {
-        free(date_string_p);
-        free(str_int);
-        return NULL;
-    }
-    str_int = str_year;
-    memcpy(str_int, tmp, 4);
-    str_int[YEAR_FORMAT] = '\0';
-    int year = atoi(str_int);
-    correct_date_input = correct_date_input && (year >= MIN_YEAR_VALUE);
-    if (!correct_date_input) {
-        free(str_int);
-        free(date_string_p);
-        return NULL;
-    }
-    struct tm *date = (struct tm *)malloc(sizeof(struct tm));
-    if (!date) {
-        free(str_int);
-        free(date_string_p);
-        return NULL;
-    }
-    struct tm tmp_date = {
-            .tm_year = year - 1900,
-            .tm_mon = month - 1,
-            .tm_mday = day
-    };
-    *date = tmp_date;
-    free(str_int);
-    free(date_string_p);
-    return date;
-}
-
 int input_number() {
     char* string = input_string();
     char *end;
